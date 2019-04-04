0bb3a098687452498be16740515ed8d1f399abaa
Naomi Cox <naomicox@mail.weber.edu>
2019-04-03 22:59:11 -0600

    added database again .sqlite
    new database

--- app.py
+++ app.py
@@ -1,65 +1,56 @@
+import sqlite3
+import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
 
-from flask import Flask, render_template
+PATH = 'db/BookData.sqlite'
 
-# create the application object
 app = Flask(__name__)
 
-# use decorators to link the function to a url
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+        cursor.close()
+        return results
+
+@app.teardown_appcontext
+def close_connection(exeption):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
 @app.route('/')
-# def home():
-#     return "Hello, World!"  # return a string
+@app.route('/books')
 
 @app.route('/index')
 def index():
-    return render_template('index.html')  # render a template
+    return render_template('index.html')
 
 @app.route('/Manga')
 def manga():
-    return render_template('manga.html')
+    Manga = execute_sql('SELECT * FROM Manga')
+    return render_template('manga.html', Manga=manga)
 
 @app.route('/comics')
 def comics():
-    return render_template('comics.html')
+    comics = execute_sql('SELECT * FROM comics')
+    return render_template('comics.html', comics=comics)
 
 @app.route('/SciFi')
 def SciFi():
-    return render_template('SciFi.html')
-
-@app.route('/fruitsbasket')
-def fruitsbasket():
-    return render_template('fruitsbasket.html')
-
-@app.route('/deathNote')
-def deathNote():
-    return render_template('deathNote.html')
-
-@app.route('/OuranHost')
-def OuranHost():
-    return render_template('OuranHost.html')
-
-@app.route('/Watchmen')
-def Watchmen():
-    return render_template('Watchmen.html')
-
-@app.route('/Saga')
-def Saga():
-    return render_template('Saga.html')
-
-@app.route('/Batman')
-def Batman():
-    return render_template('Batman.html')
-
-@app.route('/Dune')
-def Dune():
-    return render_template('Dune.html')
-
-@app.route('/Hitchhikers')
-def Hitchhikers():
-    return render_template('Hitchhikers.html')
-
-@app.route('/Enders')
-def Enders():
-    return render_template('Enders.html')
+    SciFi = execute_sql('SELECT * FROM SciFi')
+    return render_template('SciFi.html', sciFi=SciFi)
 
 
 # start the server with the 'run()' method
